2144b8e293c295d5c35fc4d060b929bd81d2d66d
Luis00809 <davidcarvajal008@gmail.com>
2024-01-04 19:10:02 -0800

    initial commit and navbar set up

---
+++ roommates/app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar"
+
+export default function HomePage() {
+  return (
+    
+    <main>
+      <Navbar />
+        <div className="row"> 
+            <h2>Welcome to Find-A-Roomie!</h2>
+            <p>If you're looking for a roommate or want to post a room in the Fresno county theres no better place to do so!</p>                
+
+        </div>
+    </main>
+)
+}
