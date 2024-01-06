1fc62d911949afea8115684e1816a211f0ead5f6
Luis00809 <davidcarvajal008@gmail.com>
2024-01-05 19:10:03 -0800

    fixes linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 
 export default function HomePage() {
   return (
-    
     <main>
         <div className="row"> 
             <h2>Welcome to Find-A-Roomie!</h2>
             <p>If you're looking for a roommate or want to post a room in the Fresno county theres no better place to do so!</p>                
-
         </div>
     </main>
 )
