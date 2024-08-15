a1d3f9314523f99c2dbdd740feca5688bc7002ed
lcarbajal <lcarbajal@teampbs.com>
2024-08-15 16:35:15 -0700

    fixes styling on room card

--- app/Components/Cards/RoomCardGrid.tsx
+++ app/Components/Cards/RoomCardGrid.tsx
@@ -13,17 +13,18 @@ export default function RoomCardGrid ({
     id: string;
 }) {
     return (
-        <Link href={`/rooms/${id}`} className="hover:scale-125 transition-transform w-1/5 no-underline text-black">
+        <Link href={`/rooms/${id}`} className="w-full h-4/5 no-underline text-black ">
             <div className="card  shadow-xl bg-red-50">
                 <div className="w-full justify-center ">
                     <img className="rounded-lg w-full h-full" src="https://www.erfurt.com/fileadmin/user_upload/tipps-inspirationen/tipps-tricks/raumwirkung/Leerer-weisser-Raum_620x417px.jpg" alt="Shoes" />
                 </div>
-                <div className="flex flex-col p-4">
+                <div className="flex flex-col ml-3">
                     <h2 className="card-title font-bold">{title}</h2>
-                    <p>{about}</p>
-                    <p>Rent: {amount}</p>
+                    <p className="multi-line-ellipsis">{about}</p>                    
+                    <p className="bg-blue-200 w-12 text-center rounded-xl p-1">${amount}</p>
                 </div>
             </div>
         </Link>
+        
     )
 }
--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -3,14 +3,13 @@ import { getRooms } from '../lib/data';
 import RoomCardGrid from '../Components/Cards/RoomCardGrid';
 
 export default async function Rooms() {
-
+    // flex flex-wrap justify-center gap-4
     const rooms = await getRooms();
-        //   className='flex flex-wrap gap-4 justify-center '
     return (
         <>
             <h2>Rooms:</h2>
             <div className='container'>
-                <div className='flex flex-wrap justify-center gap-4 '>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-4 x'>
                     {rooms?.map(room => (
                         <RoomCardGrid key={room.id}
                         title={room.address} about={room.description} 
